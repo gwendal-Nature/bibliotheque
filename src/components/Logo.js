e140c85ce2622494696b8a590e77392093a65668
Gwendal_NATURE <gwendal.nature@supdevinci-edu.fr>
2023-04-04 15:36:32 +0200

    page web avec logo dc comics

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <img src='' alt='' />
+            <h3> comics world</h3>
+
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/pages/Cherche.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const cherche = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>tu pourras trouvé tout les livre ici</h1>
+
+        </div>
+    );
+};
+
+export default cherche;
