c8c0543024ed157b680accd1b5b6e4f69dda5499
Gwendal_NATURE <gwendal.nature@supdevinci-edu.fr>
2023-04-11 09:34:02 +0200

    dernireè version a jour fonctionant

---
+++ src/components/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div className='Book'>
+            <h1>Vos livres</h1>
+        </div>
+    );
+};
+
+export default Countries;
